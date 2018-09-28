a533f5ddfa6961c87a4ad32c6b5db58f241d88e2
idobra <mobiprog@ukr.net>
2018-09-28 09:50:51 +0300

    design 1

--- app/src/main/java/ua/uz/vopak/brb4/brb4/MainActivity.java
+++ app/src/main/java/ua/uz/vopak/brb4/brb4/MainActivity.java
@@ -1,14 +1,10 @@
 package ua.uz.vopak.brb4.brb4;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
-
 import com.journeyapps.barcodescanner.BarcodeView;
-
-import ua.uz.vopak.brb4.brb4.fragments.ScanFragment;
 
 public class MainActivity extends FragmentActivity implements View.OnClickListener{
     Button btnRestart;
@@ -24,7 +20,7 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 
     @Override
     public void onClick(View v) {
-        BarcodeView barcodeView = (BarcodeView) findViewById(R.id.barcode_scanner);
+        BarcodeView barcodeView = findViewById(R.id.barcode_scanner);
         barcodeView.resume();
     }
 
