0bf94334069577ded3e3f7fd21903cc7bb03634f
idobra <mobiprog@ukr.net>
2018-09-28 12:29:44 +0300

    a

--- app/src/main/java/ua/uz/vopak/brb4/brb4/fragments/ScanFragment.java
+++ app/src/main/java/ua/uz/vopak/brb4/brb4/fragments/ScanFragment.java
@@ -143,8 +143,8 @@ public class ScanFragment extends Fragment {
             priceText.setTextColor(Color.parseColor("#3bb46e"));
         }
 
-        oldPriceView.setText(Double.toString(LI.OldPrice/100));
-        priceView.setText(Double.toString(LI.Price/100));
+        oldPriceView.setText(String.format("%.2f",(double)LI.OldPrice/100));
+        priceView.setText(String.format("%.2f",(double)LI.Price/100));
         textBarcodeView.setText(LI.BarCode);
 
 
