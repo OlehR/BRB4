19f3edf57ddb18a952cde31b8890ddbd2db594b4
idobra <mobiprog@ukr.net>
2018-10-01 18:26:19 +0300

    integrated sdk

--- app/src/main/java/ua/uz/vopak/brb4/brb4/MainActivity.java
+++ app/src/main/java/ua/uz/vopak/brb4/brb4/MainActivity.java
@@ -7,7 +7,14 @@ import android.view.WindowManager;
 import android.widget.Button;
 import com.journeyapps.barcodescanner.BarcodeView;
 
-public class MainActivity extends FragmentActivity implements View.OnClickListener{
+import com.symbol.emdk.EMDKManager;
+import com.symbol.emdk.EMDKManager.EMDKListener;
+import com.symbol.emdk.barcode.ScanDataCollection;
+import com.symbol.emdk.barcode.Scanner.DataListener;
+import com.symbol.emdk.barcode.Scanner.StatusListener;
+import com.symbol.emdk.barcode.StatusData;
+
+public class MainActivity extends FragmentActivity implements View.OnClickListener, EMDKListener, StatusListener, DataListener{
     Button btnRestart;
 
     @Override
@@ -25,6 +32,23 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     public void onClick(View v) {
         BarcodeView barcodeView = findViewById(R.id.barcode_scanner);
         barcodeView.resume();
+    }
+
+    @Override
+    public void onClosed() {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void onOpened(EMDKManager emdkManager) {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void onData(ScanDataCollection scanDataCollection) {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void onStatus(StatusData statusData) {
+        // TODO Auto-generated method stub
     }
 
 }
