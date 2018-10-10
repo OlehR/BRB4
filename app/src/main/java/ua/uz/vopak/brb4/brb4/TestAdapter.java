8dcc367d521c83719ffe797540a43262f3da6358
OlehR <OlehRut@gmail.com>
2018-10-10 09:18:20 +0300

    Start SQLite

---
+++ app/src/main/java/ua/uz/vopak/brb4/brb4/TestAdapter.java
@@ -0,0 +1,101 @@
+package ua.uz.vopak.brb4.brb4;
+
+import java.io.IOException;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+public class TestAdapter
+{
+    protected static final String TAG = "DataAdapter";
+
+    private final Context mContext;
+    private SQLiteDatabase mDb;
+    private DataBaseHelper mDbHelper;
+
+    public TestAdapter(Context context)
+    {
+        this.mContext = context;
+        mDbHelper = new DataBaseHelper(mContext);
+    }
+
+    public TestAdapter createDatabase() throws SQLException
+    {
+        try
+        {
+            mDbHelper.createDataBase();
+        }
+        catch (IOException mIOException)
+        {
+            Log.e(TAG, mIOException.toString() + "  UnableToCreateDatabase");
+            throw new Error("UnableToCreateDatabase");
+        }
+        return this;
+    }
+
+    public TestAdapter open() throws SQLException
+    {
+        try
+        {
+            mDbHelper.openDataBase();
+            mDbHelper.close();
+            mDb = mDbHelper.getReadableDatabase();
+        }
+        catch (SQLException mSQLException)
+        {
+            Log.e(TAG, "open >>"+ mSQLException.toString());
+            throw mSQLException;
+        }
+        return this;
+    }
+
+    public void close()
+    {
+        mDbHelper.close();
+    }
+
+    @Override
+    public void addContact(String parBarCode,Integer paris_good) {
+        SQLiteDatabase db = mDb;
+        ContentValues values = new ContentValues();
+        values.put("bar_code", parBarCode);
+        values.put("is_good", paris_good);
+
+        db.insert(TABLE_CONTACTS, null, values);
+        db.close();
+    }
+
+    public Cursor getTestData()
+    {
+        try
+        {
+            String sql ="SELECT * FROM myTable";
+
+            Cursor mCur = mDb.rawQuery(sql, null);
+            if (mCur!=null)
+            {
+                mCur.moveToNext();
+            }
+            return mCur;
+        }
+        catch (SQLException mSQLException)
+        {
+            Log.e(TAG, "getTestData >>"+ mSQLException.toString());
+            throw mSQLException;
+        }
+    }
+}
+
+/*
+TestAdapter mDbHelper = new TestAdapter(urContext);
+mDbHelper.createDatabase();
+mDbHelper.open();
+
+Cursor testdata = mDbHelper.getTestData();
+
+mDbHelper.close();
+ */
