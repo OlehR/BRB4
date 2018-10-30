f86bf8f1fa1dffa034a93fed5048d9118e30df07
idobra <mobiprog@ukr.net>
2018-10-30 18:28:08 +0200

    sd

--- app/src/main/java/ua/uz/vopak/brb4/brb4/RevisionActivity.java
+++ app/src/main/java/ua/uz/vopak/brb4/brb4/RevisionActivity.java
@@ -1,7 +1,9 @@
 package ua.uz.vopak.brb4.brb4;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -13,74 +15,96 @@ import ua.uz.vopak.brb4.brb4.helpers.AsyncHelpers.AsyncRevisionHelper;
 import ua.uz.vopak.brb4.brb4.models.GlobalConfig;
 
 public class RevisionActivity extends Activity {
+    TableLayout tl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.revision_layout);
+        tl = (TableLayout) findViewById(R.id.RevisionsList);
         new AsyncRevisionHelper(GlobalConfig.GetWorker(), this).execute();
     }
 
-    public void renderTable(String result){
+    public void renderTable(final String result){
+        final RevisionActivity context = this;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
 
-        try {
-            JSONObject jObject = new JSONObject(result);
+                try {
+                    JSONObject jObject = new JSONObject(result);
 
-            if(jObject.getInt("State") == 1){
-                JSONArray arrJson = jObject.getJSONArray("ListInventory");
+                    if (jObject.getInt("State") == 1) {
+                        JSONArray arrJson = jObject.getJSONArray("ListInventory");
 
-                TableLayout tl = (TableLayout) findViewById(R.id.RevisionsList);
+                        for (int i = 0; i < arrJson.length(); i++) {
+                            JSONArray innerArr = arrJson.getJSONArray(i);
 
-                for(int i = 0; i < arrJson.length(); i++) {
-                    JSONArray innerArr = arrJson.getJSONArray(i);
+                            String date = innerArr.getString(0);
+                            String numberInv = innerArr.getString(1);
+                            String extInfo = innerArr.getString(2);
+                            String warehouseNumber = innerArr.getString(3);
+                            String userName = innerArr.getString(4);
 
-                    String date = innerArr.getString(0);
-                    String numberInv = innerArr.getString(1);
-                    String extInfo = innerArr.getString(2);
-                    String warehouseNumber = innerArr.getString(3);
-                    String userName = innerArr.getString(4);
+                            TableRow tr = new TableRow(context);
+                            tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
 
-                    TableRow tr = new TableRow(this);
-                    tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+                            TableRow tr1 = new TableRow(context);
+                            tr1.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
 
-                    TableRow tr1 = new TableRow(this);
-                    tr1.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+                            TableRow tr2 = new TableRow(context);
+                            tr2.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
 
-                    TableRow tr2 = new TableRow(this);
-                    tr2.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+                            TextView Date = new TextView(context);
+                            Date.setPadding(3, 3, 3, 3);
+                            Date.setBackground(ContextCompat.getDrawable(context, R.drawable.table_cell_border));
+                            Date.setText(date);
+                            Date.setTextColor(Color.parseColor("#000000"));
+                            tr.addView(Date);
 
-                    TextView Date = new TextView(this);
-                    Date.setPadding(5, 5, 5, 5);
-                    Date.setText(date);
-                    tr.addView(Date);
+                            TextView NumberInv = new TextView(context);
+                            NumberInv.setPadding(3, 3, 3, 3);
+                            NumberInv.setBackground(ContextCompat.getDrawable(context, R.drawable.table_cell_border));
+                            NumberInv.setText(numberInv);
+                            NumberInv.setTextColor(Color.parseColor("#000000"));
+                            tr.addView(NumberInv);
 
-                    TextView NumberInv = new TextView(this);
-                    NumberInv.setPadding(5, 5, 5, 5);
-                    NumberInv.setText(numberInv);
-                    tr.addView(NumberInv);
+                            TextView ExtInfo = new TextView(context);
+                            ExtInfo.setPadding(3, 3, 3, 3);
+                            ExtInfo.setBackground(ContextCompat.getDrawable(context, R.drawable.table_cell_border));
+                            ExtInfo.setText(extInfo);
+                            ExtInfo.setTextColor(Color.parseColor("#000000"));
+                            tr1.addView(ExtInfo);
 
-                    TextView ExtInfo = new TextView(this);
-                    ExtInfo.setPadding(5, 5, 5, 5);
-                    ExtInfo.setText(extInfo);
-                    tr1.addView(ExtInfo);
+                            TextView WarehouseNumber = new TextView(context);
+                            WarehouseNumber.setPadding(3, 3, 3, 3);
+                            WarehouseNumber.setBackground(ContextCompat.getDrawable(context, R.drawable.table_cell_border));
+                            WarehouseNumber.setText(warehouseNumber);
+                            WarehouseNumber.setTextColor(Color.parseColor("#000000"));
+                            tr1.addView(WarehouseNumber);
 
-                    TextView WarehouseNumber = new TextView(this);
-                    WarehouseNumber.setPadding(5, 5, 5, 5);
-                    WarehouseNumber.setText(warehouseNumber);
-                    tr1.addView(WarehouseNumber);
+                            TextView UserName = new TextView(context);
+                            UserName.setPadding(3, 3, 3, 3);
+                            UserName.setBackground(ContextCompat.getDrawable(context, R.drawable.table_cell_border));
+                            UserName.setText(userName);
+                            UserName.setTextColor(Color.parseColor("#000000"));
 
-                    TextView UserName = new TextView(this);
-                    UserName.setPadding(5, 5, 5, 5);
-                    UserName.setText(userName);
-                    tr2.addView(UserName);
+                            TableRow.LayoutParams params = (TableRow.LayoutParams)tr2.getLayoutParams();
+                            params.span = 2;
+                            tr2.setLayoutParams(params);
 
-                    tl.addView(tr);
-                    tl.addView(tr1);
-                    tl.addView(tr2);
+                            tr2.addView(UserName);
+
+                            tl.addView(tr);
+                            tl.addView(tr1);
+                            tl.addView(tr2);
+                        }
+                    }
+
+                } catch (Exception e) {
+                    e.getMessage();
                 }
-            }
 
-        }catch (Exception e){
-            e.getMessage();
-        }
+            }
+        });
     }
 }
