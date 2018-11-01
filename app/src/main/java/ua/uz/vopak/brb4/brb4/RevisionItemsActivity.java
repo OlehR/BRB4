c5a1a3e51693829d1c9813a75eca629444fde0ee
idobra <mobiprog@ukr.net>
2018-11-01 16:27:03 +0200

    Revision Items

---
+++ app/src/main/java/ua/uz/vopak/brb4/brb4/RevisionItemsActivity.java
@@ -0,0 +1,12 @@
+package ua.uz.vopak.brb4.brb4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RevisionItemsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Додати лейаут
+    }
+}
