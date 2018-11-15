f4d87c82f6a119f4bc3042591ebfa23555dd56b1
idobra <mobiprog@ukr.net>
2018-11-15 14:23:14 +0200

    Add new scaner

--- app/src/main/java/ua/uz/vopak/brb4/brb4/RevisionScannerActivity.java
+++ app/src/main/java/ua/uz/vopak/brb4/brb4/RevisionScannerActivity.java
@@ -1,12 +1,108 @@
 package ua.uz.vopak.brb4.brb4;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import device.common.DecodeResult;
+import device.common.DecodeStateCallback;
+import device.common.ScanConst;
+import device.sdk.ScanManager;
+import ua.uz.vopak.brb4.brb4.helpers.EMDKWrapper;
+import ua.uz.vopak.brb4.brb4.helpers.mScanerWrapper;
 
 public class RevisionScannerActivity extends Activity {
+    EMDKWrapper emdkWrapper;
+    static mScanerWrapper mScanerW;
+    private final Handler mHandler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.revision_scanner_activity);
+
+        String model = android.os.Build.MODEL;
+        if( model.equals("TC20") && ( android.os.Build.MANUFACTURER.contains("Zebra Technologies") || android.os.Build.MANUFACTURER.contains("Motorola Solutions")) ){
+            emdkWrapper  = new EMDKWrapper(getApplicationContext());
+        }
+
+        if( model.equals("PM550") && android.os.Build.MANUFACTURER.contains("Point Mobile Co., Ltd.")){
+            mScanerW  = new mScanerWrapper();
+            mScanerW.mContext = this;
+            mScanerW.mScanner = new ScanManager();
+            mScanerW.mDecodeResult = new DecodeResult();
+            mScanerW.mScanner.aDecodeSetResultType(ScanConst.ResultType.DCD_RESULT_EVENT);
+        }
+    }
+
+    public static class ScanResultReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(mScanerW != null) {
+                if (ScanConst.INTENT_USERMSG.equals(intent.getAction())) {
+                    mScanerW.mScanner.aDecodeGetResult(mScanerW.mDecodeResult.recycle());
+                }else if (ScanConst.INTENT_EVENT.equals(intent.getAction())) {
+                    byte[] decodeBytesValue = intent.getByteArrayExtra(ScanConst.EXTRA_EVENT_DECODE_VALUE);
+                    String value = new String(decodeBytesValue);
+
+                    String a = value;
+
+                }
+            }
+
+        }
+    }
+
+    private DecodeStateCallback mStateCallback = new DecodeStateCallback(mHandler) {
+        public void onChangedState(int state) {
+            switch (state) {
+                case ScanConst.STATE_ON:
+                case ScanConst.STATE_TURNING_ON:
+
+                    break;
+                case ScanConst.STATE_OFF:
+                case ScanConst.STATE_TURNING_OFF:
+
+                    break;
+            }
+        };
+    };
+
+    private void initScanner() {
+        if (mScanerW != null) {
+            mScanerW.mScanner.aRegisterDecodeStateCallback(mStateCallback);
+            mScanerW.mScanner.aDecodeSetResultType(ScanConst.ResultType.DCD_RESULT_USERMSG);
+        }
+    }
+
+    private Runnable mStartOnResume = new Runnable() {
+        @Override
+        public void run() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    initScanner();
+                }
+            });
+        }
+    };
+
+    @Override
+    protected void onPause() {
+        if (mScanerW.mScanner != null) {
+            mScanerW.mScanner.aUnregisterDecodeStateCallback(mStateCallback);
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mScanerW != null) {
+            mScanerW = null;
+        }
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/ua/uz/vopak/brb4/brb4/helpers/mScanerWrapper.java
@@ -0,0 +1,12 @@
+package ua.uz.vopak.brb4.brb4.helpers;
+
+import android.content.Context;
+
+import device.common.DecodeResult;
+import device.sdk.ScanManager;
+
+public class mScanerWrapper {
+    public static ScanManager mScanner;
+    public static DecodeResult mDecodeResult;
+    public Context mContext;
+}
