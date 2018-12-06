3b49c56e3993ad4e5d3a5608361368fd8bcd1058
idobra <mobiprog@ukr.net>
2018-12-06 19:29:58 +0200

    Закінчена робота на прайсчекером для клієнта

--- clientpricechecker/src/main/java/ua/uz/vopak/brb4/clientpricechecker/AsyncPriceDataHelper.java
+++ clientpricechecker/src/main/java/ua/uz/vopak/brb4/clientpricechecker/AsyncPriceDataHelper.java
@@ -1,4 +1,40 @@
 package ua.uz.vopak.brb4.clientpricechecker;
 
-public class AsyncPriceDataHelper {
+import android.os.AsyncTask;
+
+public class AsyncPriceDataHelper extends AsyncTask<String , Void, LabelInfo>
+{
+    ClientPriceCheckerActivity activity;
+    @Override
+    protected LabelInfo doInBackground(String... param)
+    {
+        GetDataHTTP Http = new GetDataHTTP();
+        LabelInfo LI = new LabelInfo();
+        String BarCode = param[0].replace("\n","");
+        Config config = Config.instance();
+
+        String resHttp=Http.GetData(config.getCodeWarehouse(),BarCode,"");
+        resHttp=resHttp.replace("&amp;","&");
+
+        LI.InfoHTTP= Http.HttpState.name();
+        if(resHttp!=null && !resHttp.isEmpty())
+        {
+            LI.Init(resHttp);
+        }
+
+        return LI;
+    }
+
+    @Override
+    protected void onPostExecute(LabelInfo parLI)
+    {
+        activity.setScanResult(parLI);
+    };
+
+
+    public AsyncPriceDataHelper( ClientPriceCheckerActivity context)
+    {
+        activity=context;
+    }
+
 }
--- clientpricechecker/src/main/java/ua/uz/vopak/brb4/clientpricechecker/ClientPriceCheckerActivity.java
+++ clientpricechecker/src/main/java/ua/uz/vopak/brb4/clientpricechecker/ClientPriceCheckerActivity.java
@@ -1,20 +1,38 @@
 package ua.uz.vopak.brb4.clientpricechecker;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ClientPriceCheckerActivity extends Activity {
+    TextView Title, BarCodeView, Article, ActionView, PriceBill, PriceCoin;
+    RelativeLayout InfoLayout;
+    LinearLayout LogoLayout;
+    ImageView Background;
+    ClientPriceCheckerActivity context;
     LinearLayout ClientPriceChecker;
     EditText BarCode;
+    private Timer infoLayoutTimer;
+    private InfoLayoutTimerTask infoLayoutTimerTask;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = this;
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -29,6 +47,7 @@ public class ClientPriceCheckerActivity extends Activity {
                     if(s.toString().indexOf("\n") > 0){
                         String barCode = BarCode.getText().toString();
                         BarCode.setText("");
+                        new AsyncPriceDataHelper(context).execute(barCode);
                     }
             }
 
@@ -40,5 +59,69 @@ public class ClientPriceCheckerActivity extends Activity {
             }
         });
 
+        InfoLayout = findViewById(R.id.InfoLayout);
+        Title = findViewById(R.id.Title);
+        BarCodeView = findViewById(R.id.BarCodeView);
+        Article = findViewById(R.id.Article);
+        ActionView = findViewById(R.id.Action);
+        PriceBill = findViewById(R.id.PriceBill);
+        PriceCoin = findViewById(R.id.PriceBill);
+        Background = findViewById(R.id.Background);
+        LogoLayout = findViewById(R.id.LogoLayout);
+
+    }
+
+    public void setScanResult(LabelInfo Li){
+        Title.setText(Li.Name);
+        BarCodeView.setText(Li.BarCode);
+        Article.setText(Li.Article);
+        PriceBill.setText(((Integer)Li.PriceBill).toString());
+        PriceCoin.setText(((Integer)Li.PriceCoin).toString());
+
+        if(Li.Action){
+            ActionView.setVisibility(View.VISIBLE);
+        }else {
+            ActionView.setVisibility(View.INVISIBLE);
+        }
+
+        Resources res = getResources();
+        Drawable background = res.getDrawable(R.drawable.background_3);
+
+        Background.setBackground(background);
+        LogoLayout.setVisibility(View.INVISIBLE);
+        InfoLayout.setVisibility(View.VISIBLE);
+
+        if (infoLayoutTimer != null) {
+            infoLayoutTimer.cancel();
+        }
+
+        infoLayoutTimer = new Timer();
+        infoLayoutTimerTask = new InfoLayoutTimerTask();
+
+        infoLayoutTimer.schedule(infoLayoutTimerTask, 30000);
+
+    }
+
+    public void hideInfo(){
+        InfoLayout.setVisibility(View.INVISIBLE);
+        LogoLayout.setVisibility(View.VISIBLE);
+        Resources res = getResources();
+        Drawable background = res.getDrawable(R.drawable.background_4);
+        Background.setBackground(background);
+    }
+
+    class InfoLayoutTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+
+            runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    hideInfo();
+                }
+            });
+        }
     }
 }
