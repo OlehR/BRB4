dad55ae2f65d1588e34efe081a38e3a8747f99f6
idobra <mobiprog@ukr.net>
2019-02-27 09:57:19 +0200

    Add Weigth activity

---
+++ app/src/main/java/ua/uz/vopak/brb4/brb4/DocumentWeightActivity.java
@@ -0,0 +1,11 @@
+package ua.uz.vopak.brb4.brb4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DocumentWeightActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
